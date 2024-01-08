cd067861db8bbd902453a37e76f63d3b6798b8cd
Thanin <chea14chainat@gmail.com>
2024-01-08 15:58:19 +0700

    update

--- src/components/Card/CardWork.jsx
+++ src/components/Card/CardWork.jsx
@@ -6,7 +6,9 @@ function CardWork({ image, title, link }) {
   return (
     <div>
       {/* Use an anchor tag to wrap the Card */}
-      <a href={link} className={styles.cardLink}>
+      {/* <a href={link} className={styles.cardLink}> */}
+      <a  className={styles.cardLink}>
+
         <Card className={styles.card} style={{width: "40rem", border: "none", margin: "1rem" }}>
           <Card.Img variant="top" src={image} className={styles.image} />
           <Card.Body className={styles.body}>
